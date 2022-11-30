a05896a6b0f76358e6f49e4ac77cedfa82fb4f32
unknown <osama_magdy0@yahoo.com>
2022-11-30 21:45:24 +0200

    Upload from home

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const btnEl = document.querySelector(".btn");
+const characters =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+// console.log();
+// console.log(num);
 
+btnEl.addEventListener("click", function () {
+  const num = Math.ceil(Math.random() * 62);
+
+  for (let i = 0; i < 16; i++) {
+    console.log(i);
+  }
+
+  // console.log(characters[num]);
+});
